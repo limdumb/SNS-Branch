4f29614fd935c4c90e88bb4318d2f7696d781c08
limdumb <dlaruddls@nate.com>
2023-11-23 17:30:36 +0900

    feat: Font 분리 및 Nobile 폰트 추가

---
+++ src/font.ts
@@ -0,0 +1,5 @@
+import { Inter, Lusitana, Nobile } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const lusitana = Lusitana({ weight: "400", subsets: ["latin"] });
+export const nobile = Nobile({ weight: "400", subsets: ["latin"] });
