6cc1fc8d4a581f3c633e77d83d82fb956c4468d2
limdumb <dlaruddls@nate.com>
2023-11-27 15:23:56 +0900

    misc: 로고폰트 추가

--- src/font.ts
+++ src/font.ts
@@ -1,5 +1,6 @@
-import { Inter, Lusitana, Nobile } from "next/font/google";
+import { Inter, Kanit, Lusitana, Nobile } from "next/font/google";
 
 export const inter = Inter({ subsets: ["latin"] });
 export const lusitana = Lusitana({ weight: "400", subsets: ["latin"] });
 export const nobile = Nobile({ weight: "400", subsets: ["latin"] });
+export const kanit = Kanit({ weight: "400", subsets: ["latin"] });
