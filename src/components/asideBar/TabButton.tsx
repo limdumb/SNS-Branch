1cf725789c9ff9c77c54220d56a43704f6f2d19a
limdumb <dlaruddls@nate.com>
2023-11-28 16:44:37 +0900

    feat: TabButton 컴포넌트 생성

---
+++ src/components/asideBar/TabButton.tsx
@@ -0,0 +1,22 @@
+import iconValue from "@/function/util/iconValue";
+import style from "./style/tabButton.module.css";
+import { signika } from "@/font";
+
+interface TabButtonProps {
+  contents: "Home" | "Friends" | "Commercial" | "Profile" | "Messages";
+}
+
+export default function TabButton(props: TabButtonProps) {
+  const icons = iconValue();
+
+  return (
+    <div className={`${style.Tab_Button_Container} ${signika.className}`}>
+      {icons.map((el) => {
+        if (el.name === props.contents) {
+          return el.icon;
+        }
+      })}
+      <button className={style.Tab_Button_Container}>{props.contents}</button>
+    </div>
+  );
+}
