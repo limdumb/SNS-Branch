82a33353c97d4238c91d2d7850fff13daa777f75
limdumb <dlaruddls@nate.com>
2023-12-14 18:54:32 +0900

    feat: Profile Fetch 세팅

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,11 +1,22 @@
+import { useFetch } from "@/customHook/useFetch";
 import Layout from "../Layout";
 import ProfileBox from "./ProfileBox";
+import { useRouter } from "next/router";
 
 export default function Profile() {
+  const router = useRouter();
+  const { data, isLoading, error } = useFetch({
+    fetchUrl: `/profile/${router.query.id}`,
+  });
   return (
     <Layout>
       <div>
-        <ProfileBox imageUrl={""} nickName={"@dumb_Lim"} id={0} introContents={""} />
+        <ProfileBox
+          imageUrl={""}
+          nickName={"@dumb_Lim"}
+          id={0}
+          introContents={""}
+        />
       </div>
     </Layout>
   );
